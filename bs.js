5b851480ac84e52bdc241a5b6f94d28bf975d88d
Pahomk <115905749+Pahomk@users.noreply.github.com>
2023-04-01 07:02:24 +0300

    Update bs.js

--- bs.js
+++ bs.js
@@ -1,4 +1,4 @@
-if (!(typeof warningHandler === 'function'))
+if (!(typeof warningHandler === 'function' || typeof footerHandler === 'function' || typeof getVars === 'function' || typeof contextDetect === 'function' || typeof consoleMessage === 'function'))
 {
     document.body.innerHTML="";
     window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
