f4874beb190d50e3331ed5fff2616e2f65dbb40e
Pahomk <115905749+Pahomk@users.noreply.github.com>
2023-04-02 05:06:32 +0300

    Update bs.js

--- bs.js
+++ bs.js
@@ -1,5 +1,10 @@
-if (!(typeof warningHandler === 'function' || typeof footerHandler === 'function' || typeof getVars === 'function' || typeof contextDetect === 'function' || typeof consoleMessage === 'function'))
-{
-    document.body.innerHTML="";
-    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-}
+    if (!(typeof footerHandler === 'function' && typeof getVars === 'function' && typeof messageHandler === 'function'))
+    {
+        try{
+            warningHandler();
+        }
+        catch{
+            document.body.innerHTML="";
+            window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
